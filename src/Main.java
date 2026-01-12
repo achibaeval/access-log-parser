6492e2c66376a2b0c0a3cf87df76c5640b5b0ed1
Liliya <achibaeval@gmail.com>
2026-01-12 22:59:06 +0300

    Add two-number operations

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
 
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int first = sc.nextInt();
+
+        System.out.println("Введите второе число:");
+        int second = sc.nextInt();
+
+        int sum = first + second;
+        int diff = first - second;
+        int product = first * second;
+        double quotient = (double) first / second;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + diff);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
