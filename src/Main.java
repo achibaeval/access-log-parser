3c771485948e364ca21777c903d6d5f871e89598
Liliya <achibaeval@gmail.com>
2026-01-12 22:45:40 +0300

    Add text length calculator and jar build

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
 
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+        System.out.println("Длина текста: " + text.length());
     }
 }
